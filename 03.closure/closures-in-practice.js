f87d6ca48ac2a95790a3a1c10ab25cbfa6064cc3
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-07-24 16:47:37 -0300

    add example for use closure

--- 03.closure/closures-in-practice.js
+++ 03.closure/closures-in-practice.js
@@ -1,11 +1,26 @@
 //* closure pode ser usado quando é preciso repetir a lógica, mudando apenas os valores bases da função
 
-function createMultiplier(operator) {
-  return (number) => number * operator;
+// function createMultiplier(operator) {
+//   return (number) => number * operator;
+// }
+
+// const double = createMultiplier(2);
+// const triple = createMultiplier(3);
+
+// console.log(double(5));
+// console.log(triple(5));
+
+function myfunction1(data) {
+  let _data = data;
+  return {
+    getData() {
+      console.log(_data);
+    },
+    setData(newData) {
+      _data = newData;
+      console.log(_data);
+    },
+  };
 }
 
-const double = createMultiplier(2);
-const triple = createMultiplier(3);
-
-console.log(double(5));
-console.log(triple(5));
+const createData = myfunction1([{ name: "Vinícius", age: 23 }]);

16e0acb6c544f7d31fad76f6c083982f0acc686f
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-07-31 16:06:09 -0300

    prototype

--- 05.prototype/prototype-shared-methods.js
+++ 05.prototype/prototype-shared-methods.js
@@ -16,27 +16,44 @@ function Car(name, color, speed) {
   };
 }
 
-const bmw = new Car("BMW", "blue", "80km/h");
-const civic = new Car("Civic", "black", "120km/h");
-const corsa = new Car("Corsa", "red", "600km/h");
+function Bmw(name, color, speed) {
+  Car.call(this, name, color, speed);
+}
+Bmw.prototype = Object.create(Car.prototype);
+Bmw.prototype.constructor = Bmw;
 
-Object.setPrototypeOf(bmw, Car);
-Object.setPrototypeOf(civic, Car);
-Object.setPrototypeOf(corsa, Car);
+function Civic(name, color, speed) {
+  Car.call(this, name, color, speed);
+}
+Civic.prototype = Object.create(Car.prototype);
+Civic.prototype.constructor = Civic;
+
+function Corsa(name, color, speed) {
+  Car.call(this, name, color, speed);
+}
+Corsa.prototype = Object.create(Car.prototype);
+Corsa.prototype.constructor = Corsa;
+
+const bmw = new Bmw("BMW", "blue", "80km/h");
+const civic = new Civic("Civic", "black", "120km/h");
+const corsa = new Corsa("Corsa", "red", "600km/h");
 
 console.log(bmw.showInfo());
 console.log(bmw.showColor());
 console.log(bmw.showSpeed());
-console.log(Object.getPrototypeOf(bmw));
+console.log("Prototype:", Object.getPrototypeOf(bmw));
+console.log("Constructor:", Object.getPrototypeOf(bmw).constructor);
 console.log();
 
 console.log(civic.showInfo());
 console.log(civic.showColor());
 console.log(civic.showSpeed());
-console.log(Object.getPrototypeOf(civic));
+console.log("Prototype:", Object.getPrototypeOf(civic));
+console.log("Constructor:", Object.getPrototypeOf(civic).constructor);
 console.log();
 
 console.log(corsa.showInfo());
 console.log(corsa.showColor());
 console.log(corsa.showSpeed());
-console.log(Object.getPrototypeOf(corsa));
+console.log("Prototype:", Object.getPrototypeOf(corsa));
+console.log("Constructor:", Object.getPrototypeOf(corsa).constructor);
